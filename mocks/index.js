6f992a0aecf7327545875a7a67e5d38c005ef17a
ziyuanzhang <ziyuzhan@cisco.com>
2017-12-27 11:16:10 +0800

    提交模拟后台接口文件

---
+++ mocks/index.js
@@ -0,0 +1,13 @@
+/**
+ * 模拟接口文件
+ */
+module.exports = function(app) {
+    app.get("rest", function(req, content, callback) {
+        setTimeout(function() {
+            callback(null, {
+                a: 1,
+                b: 2
+            });
+        }, 500);
+    });
+};
