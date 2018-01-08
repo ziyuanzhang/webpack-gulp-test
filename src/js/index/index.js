65d63160a61ed67fdf8acf78b2894da339f48fb8
ziyuanzhang <ziyuzhan@cisco.com>
2018-01-08 17:29:53 +0800

    dd

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -9,8 +9,29 @@ function a() {
         dataType: "jsonp", //指定服务器返回的数据类型
         success: function (data) {
             //var result = JSON.stringify(data); //json对象转成字符串
-            console.log("index:",data);
+            console.log("index:", data);
         }
     });
+    var prid = "5094412400";
+    var token = "9026326a52a38bd3c6df58ae83e67fcd75c984b8fa11016f5543db6283c1130424543512af9a3a453c214e715879a55aa9bd3156f155ab1051c5fd8503de108592a32f9ec4ecf882e4d9d3854d423b08";
+    $.ajax({
+        url: "https://api.digitalriver.com/v1/shoppers/me/carts/active?productId=" + prid,
+        data: {
+            token: token,
+            format: "json",
+            method: "post",
+            quantity: "0",
+            expand: "lineItems.lineItem.product.sku",
+            testOrder: "true"
+        },
+        dataType: "jsonp",
+        error: function (data) {
+            console.log("DR:失败", data);
+        },
+        success: function (data) {
+            console.log("DR:正常", data);
+        }
+    });
+
 }
 a();
