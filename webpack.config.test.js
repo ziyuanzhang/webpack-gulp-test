f8376af4d839e189705866156d053d5ac008b568
ziyuanzhang <ziyuzhan@cisco.com>
2018-01-12 11:26:15 +0800

    优化webpack

--- webpack.config.test.js
+++ webpack.config.test.js
@@ -1,10 +1,10 @@
 /* ./node_modules/.bin/webpack --config webpack.config.test.js */
 
 var webpack = require("webpack");
-var uglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
 var srcDir = path.resolve(process.cwd(), 'src');
 module.exports = {
+    devtool: "#source-map",
     entry: {
         index: ["./src/vendor/jquery/jquery-1.7.2.min.js", "./src/js/index/index.js", "./src/js/common/common.js"],
         home: ["./src/vendor/jquery/jquery-1.7.2.min.js", "./src/js/home/home.js", "./src/js/common/common.js"]
@@ -16,14 +16,19 @@ module.exports = {
     resolve: {
         alias: {
             jquery: srcDir + "/vendor/jquery/jquery-1.7.2.min.js",
-            common: srcDir + "/js/common//common.js"
+            common: srcDir + "/js/common/common.js"
         }
     },
     plugins: [
-        new uglifyJsPlugin({
+        /* 有些JS库有自己的依赖树，并且这些库可能有交叉的依赖，DedupePlugin可以找出他们并删除重复的依赖。 */
+        new webpack.optimize.DedupePlugin(),
+        /* 解析/压缩/美化所有的js chunk */
+        new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         })
     ]
 };
+
+
